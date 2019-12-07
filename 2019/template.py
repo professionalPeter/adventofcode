b084e82d02a73bb1a299060fb48f289c2f08bf45
Peter Lewis <peter.lewis@climate.com>
2019-12-06 22:16:26 -0600

    Create a template file of all the boilerplate

---
+++ 2019/template.py
@@ -0,0 +1,29 @@
+"""Solutions for Day DAY of Advent of Code 2019
+
+https://adventofcode.com/2019/day/DAY
+"""
+
+def part1(inputs = None):
+    """Output the answer to part 1 - """
+    print(f'Part 1 answer: {None}')
+
+def part2(inputs = None):
+    """Output the answer to part 2 - """
+    print(f'Part 2 answer: {None}')
+
+def puzzle_input():
+    """Returns the official input for the puzzle"""
+    with open('day0DAYinput.txt') as file:
+        return file.read()
+
+def part1_test_input():
+    """Returns the test data set from the description of part 1"""
+    return """"""
+
+def part2_test_input():
+    """Returns the test data set from the description of part 2"""
+    return """"""
+
+if __name__ == '__main__':
+    part1()
+    part2()
