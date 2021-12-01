4d4ae8311980c40cdde31c06b0637895b921d5f7
Peter Lewis <professionalPeter@users.noreply.github.com>
2021-12-01 16:50:56 -0600

    Simplify Day 01

--- 2021/day01.py
+++ 2021/day01.py
@@ -1,35 +1,30 @@
-"""Solutions for Day 01 of Advent of Code 2019
+"""Solutions for Day 01 of Advent of Code 2021
 
-https://adventofcode.com/2019/day/01
+https://adventofcode.com/2021/day/01
 """
+import aoc
+
+def count_increases(int_list, window_size):
+    incs = 0
+    for previous, current in zip(int_list, int_list[window_size:]):
+            incs += 1 if current > previous else 0
+    return incs
 
 def part1(inputs = None):
     """Output the answer to part 1 - """
-    nums = [int(x) for x in inputs.splitlines()]
-    incs = 0
-    for previous, current in zip(nums, nums[1:]):
-            incs += 1 if current > previous else 0
-    print(f'Part 1 answer: {incs}')
+    print(f'Part 1 answer: {count_increases(inputs, 1)}')
 
 def part2(inputs = None):
     """Output the answer to part 2 - """
-    nums = [int(x) for x in inputs.splitlines()]
-    windows = []
-    for a, b, c in zip(nums, nums[1:], nums[2:]):
-        windows.append(a + b + c)
-    incs = 0
-    for previous, current in zip(windows, windows[1:]):
-            incs += 1 if current > previous else 0
-    print(f'Part 2 answer: {incs}')
+    print(f'Part 2 answer: {count_increases(inputs, 3)}')
 
 def puzzle_input():
     """Returns the official input for the puzzle"""
-    with open('day01input.txt') as file:
-        return file.read()
+    return aoc.Input.from_file('day01input.txt')
 
 def part1_test_input():
     """Returns the test data set from the description of part 1"""
-    return """199
+    return aoc.Input.from_data("""199
 200
 208
 210
@@ -38,12 +33,13 @@ def part1_test_input():
 240
 269
 260
-263"""
+263""")
 
 def part2_test_input():
     """Returns the test data set from the description of part 2"""
     return """"""
 
 if __name__ == '__main__':
-    part1(puzzle_input())
-    part2(puzzle_input())
+    part1(puzzle_input().parse_ints())
+    part2(puzzle_input().parse_ints())
+
