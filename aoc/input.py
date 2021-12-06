05378a82c106a89efc40e12cab83564ecf74ae6f
Peter Lewis <professionalPeter@users.noreply.github.com>
2021-12-06 16:10:33 -0600

    Allow the separator to specified for parse_ints

--- aoc/input.py
+++ aoc/input.py
@@ -8,8 +8,8 @@ class Input:
     def from_file(cls, filepath):
         with open(filepath) as file:
             return cls(file.read())
-    def parse_ints(self):
-        return [int(i) for i in self._data.split()]
+    def parse_ints(self, sep=None):
+        return [int(i) for i in self._data.split(sep)]
     def parse_lines(self):
         return self._data.splitlines()
     def parse_records(self):
