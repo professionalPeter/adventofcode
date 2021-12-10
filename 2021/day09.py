ee3542f5ff9bd8244eb3a54ccf387d5105a18254
Peter Lewis <peterglewis@gmail.com>
2021-12-09 23:32:48 -0600

    WIP: Day09 working k Please enter the commit message for your changes. Lines starting

--- 2021/day09.py
+++ 2021/day09.py
@@ -8,6 +8,11 @@ from collections import namedtuple, defaultdict
 
 CavePos = namedtuple("CavePos", "x y height")
 
+def prod(iterable):
+    answer = 1
+    for val in iterable:
+        answer *= val
+    return answer
 
 def safe_get(list_, index, default):
     return list_[index] if index >= 0 and index < len(list_) else default
@@ -26,29 +31,40 @@ def part1(inputs = None):
 def part2(inputs = None):
     """Output the answer to part 2 - """
     last_basin = -1
-    prev_line = ['9'] * len(inputs[0])
+    prev_line = [None] * len(inputs[0])
     print(prev_line)
     basin_sizes = defaultdict(int)
     curr_line_basins = []
+    redirect = {}
     for y, line in enumerate(inputs):
         for x, height in enumerate(line):
+            curr_basin = last_basin
             if height == '9':
                 curr_line_basins.append(None)
                 continue
             if x == 0 or line[x-1] == '9':
                 last_basin += 1
+                curr_basin = last_basin
                 print("New Basin: ", last_basin, "at ", x, ",", y)
             if prev_line[x] != None:
-                print(f'prev[{x}] = {prev_line[x]}')
-                print(f"Merging: {prev_line[x]}:{basin_sizes[prev_line[x]]} into {last_basin}:{basin_sizes[last_basin]} at {x},{y}")
-                basin_sizes[last_basin] += basin_sizes[prev_line[x]]
-                basin_sizes[prev_line[x]] = 0
-            basin_sizes[last_basin] += 1
-            #print(f'basin {last_basin} = {basin_sizes[last_basin]}')
-            curr_line_basins.append(last_basin)
+                print(f'prev[{x}] = {prev_line[x]} at {x},{y}')
+                redirect_basin = redirect.get(prev_line[x])
+                if redirect_basin:
+                    print(f'Redirect: {prev_line[x]} to {redirect_basin}')
+                    curr_basin = redirect_basin
+                else:
+                    print(f"Merging: {prev_line[x]}:{basin_sizes[prev_line[x]]} into {last_basin}:{basin_sizes[last_basin]} at {x},{y}")
+                    basin_sizes[last_basin] += basin_sizes[prev_line[x]]
+                    basin_sizes[prev_line[x]] = 0
+                    redirect[prev_line[x]] = last_basin
+            basin_sizes[curr_basin] += 1
+            #print(f'basin {curr_basin} = {basin_sizes[curr_basin]}')
+            curr_line_basins.append(curr_basin)
         prev_line = curr_line_basins
+        curr_line_basins = []
     basin_sizes ={k:v for k,v in basin_sizes.items() if v >0}
-    print(f'Part 2 answer: {basin_sizes}')
+    largest_basins = sorted(basin_sizes.values(), reverse = True)[0:3]
+    print(f'Part 2 answer: {prod(largest_basins)}')
 
 def puzzle_input():
     """Returns the official input for the puzzle"""
@@ -68,5 +84,5 @@ def test_input2():
 
 if __name__ == '__main__':
     part1(puzzle_input().parse_lines())
-    part2(test_input1().parse_lines())
+    part2(puzzle_input().parse_lines())
 
